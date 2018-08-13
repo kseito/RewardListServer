dce7f9d7ee895e3f577d81d39c7d47bff39ceba2
k-seito <torinokonoko@gmail.com>
2018-08-14 08:49:28 +0900

    add HelloController

---
+++ src/main/kotlin/kzt/project/rewardlist/HelloController.kt
@@ -0,0 +1,12 @@
+package kzt.project.rewardlist
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("hello")
+    fun hello(@RequestParam("name") name: String): String = "Hello, $name!"
+}
