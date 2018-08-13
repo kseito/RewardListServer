87dad76d92207d4da28b8fa99f79163fedf9fe80
k-seito <torinokonoko@gmail.com>
2018-08-14 08:40:33 +0900

    first commit

---
+++ src/main/kotlin/kzt/project/rewardlist/RewardlistApplication.kt
@@ -0,0 +1,11 @@
+package kzt.project.rewardlist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RewardlistApplication
+
+fun main(args: Array<String>) {
+    runApplication<RewardlistApplication>(*args)
+}
---
+++ src/test/kotlin/kzt/project/rewardlist/RewardlistApplicationTests.kt
@@ -0,0 +1,16 @@
+package kzt.project.rewardlist
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RewardlistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
