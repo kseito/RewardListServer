692ed780a7e9d240309091020879d10b8d5119ff
k-seito <torinokonoko@gmail.com>
2018-08-15 08:16:17 +0900

    add TaskCreateForm

--- src/main/kotlin/kzt/project/rewardlist/TaskController.kt
+++ src/main/kotlin/kzt/project/rewardlist/TaskController.kt
@@ -17,4 +17,9 @@ class TaskController(private val taskRepository: TaskRepository) {
         model.addAttribute("tasks", tasks)
         return "tasks/index"
     }
+
+    @GetMapping("new")
+    fun new(form: TaskCreateForm): String {
+        return "tasks/new"
+    }
 }
