585f45bf11ee700982437a5018c3fe52c04f5c4d
k-seito <torinokonoko@gmail.com>
2018-08-15 07:49:53 +0900

    add html template

--- src/main/kotlin/kzt/project/rewardlist/TaskController.kt
+++ src/main/kotlin/kzt/project/rewardlist/TaskController.kt
@@ -1,12 +1,13 @@
 package kzt.project.rewardlist
 
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("tasks")
 class TaskController {
 
@@ -16,6 +17,7 @@ class TaskController {
                 Task(1, "英単語を覚える", false),
                 Task(2, "晩御飯の食材を買う", false)
         )
-        return tasks.toString()
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
     }
 }
