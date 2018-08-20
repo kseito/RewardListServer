fde9309a5142d081b5e70c4250c1a58fccd263eb
k-seito <torinokonoko@gmail.com>
2018-08-21 07:27:20 +0900

    add database

--- src/main/kotlin/kzt/project/rewardlist/RewardlistApplication.kt
+++ src/main/kotlin/kzt/project/rewardlist/RewardlistApplication.kt
@@ -1,10 +1,24 @@
 package kzt.project.rewardlist
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class RewardlistApplication
+class RewardlistApplication {
+
+    @Bean
+    fun commandLineRunner(jdbcTemplate: JdbcTemplate) = CommandLineRunner {
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS task (
+            id      BIGINT          PRIMARY KEY AUTO_INCREMENT,
+            content VARCHAR(100)    NOT NULL,
+            done    BOOLEAN         NOT NULL DEFAULT FALSE
+            )""")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<RewardlistApplication>(*args)
